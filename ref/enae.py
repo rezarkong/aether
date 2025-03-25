b5a82f552e9d90d13bcf1a79c8caa3b0d8d9d4f1
rezarkong <rezarkong@gmail.com>
2025-03-25 23:13:20 +0800

    initial commit

--- ref/enae.py
+++ ref/enae.py
@@ -25,15 +25,24 @@ def roundupdate1(s, x0, x1, f):
     s[0], s[1], s[2], s[3], s[4], s[5] = s0, s1, s2, s3, s4, s5
 
 def roundupdate2(s, x0, x1, x2, f):
+    # s0 = _xor(f(s[8]), x0)
+    # s1 = _xor(f(s[0]), x1)
+    # s2 = _xor(f(s[1]), s[2])
+    # s3 = _xor(f(s[2]), x2)
+    # s4 = _xor(f(s[3]), s[0])
+    # s5 = _xor(f(s[4]), s[1])
+    # s6 = _xor(f(s[5]), s[3])
+    # s7 = _xor(f(s[6]), s[8])
+    # s8 = _xor(f(s[7]), s[4])
     s0 = _xor(f(s[8]), x0)
-    s1 = _xor(f(s[0]), x1)
-    s2 = _xor(f(s[1]), s[2])
-    s3 = _xor(f(s[2]), x2)
-    s4 = _xor(f(s[3]), s[0])
-    s5 = _xor(f(s[4]), s[1])
-    s6 = _xor(f(s[5]), s[3])
-    s7 = _xor(f(s[6]), s[8])
-    s8 = _xor(f(s[7]), s[4])
+    s1 = _xor(f(s[0]), s[3])
+    s2 = _xor(f(s[1]), s[6])
+    s3 = _xor(f(s[2]), x1)
+    s4 = _xor(f(s[3]), s[4])
+    s5 = _xor(f(s[4]), x2)
+    s6 = _xor(f(s[5]), s[8])
+    s7 = _xor(f(s[6]), s[2])
+    s8 = _xor(f(s[7]), s[0])
     s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8] = s0, s1, s2, s3, s4, s5, s6, s7, s8
     
 def initialization0(n, k0, k1, f):
@@ -57,13 +66,22 @@ def initialization1(n, k0, k1, f):
     return s
 
 def initialization2(n, k0, k1, f):
-    s = [ k1[:], _xor(n, k0), _z0[:], _z1[:], [0]*128,[0]*128, [0]*128, [0]*128, [0]*128, [0]*128]
-    
+    # s = [ k1[:], _xor(n, k0), _z0[:], _z1[:], [0]*128,[0]*128, [0]*128, [0]*128, [0]*128, [0]*128]
+    # s = [_z1[:], k0, n + k0, [0] * 128, _z0[:], [0] * 128, n, k1, _z2[:]]
+    s = [_z1[:], k0, _xor(n, k0), [0] * 128, _z0[:], [0] * 128, n, k1, _z2[:]]
+
     for _ in range(20):
         roundupdate2(s, _z0, _z1, _z2, f)
-
+    # print("s[9]: ", s[9])
     s[0] = _xor(s[0], k0)
-    s[1] = _xor(s[1], k1)
+    s[1] = _xor(s[1], k0)
+    s[2] = _xor(s[2], k0)
+    s[3] = _xor(s[3], k0)
+    s[4] = _xor(s[4], k1)
+    s[5] = _xor(s[5], k0)
+    s[6] = _xor(s[6], k1)
+    s[7] = _xor(s[7], k1)
+    s[8] = _xor(s[8], k1)
     return s
 
 def absorbad0(s, ad0, adlen, f):
@@ -102,12 +120,16 @@ def absorbmsg1(s, msg0, msg1, msglen, f):
     return ct
 
 def absorbmsg2(s, msg0, msg1, msg2, msglen, f):
-    func = lambda i : (
-        _xor(_xor(f(_xor(s[0], s[1])), s[3]), msg0[i]),
-        _xor(_xor(f(_xor(s[2], s[3])), s[4]), msg1[i]),
-        _xor(_xor(f(_xor(s[0], s[3])), s[7]), msg2[i]),
+    # func = lambda i : (
+    #     _xor(_xor(f(_xor(s[0], s[1])), s[3]), msg0[i]),
+    #     _xor(_xor(f(_xor(s[2], s[3])), s[4]), msg1[i]),
+    #     _xor(_xor(f(_xor(s[0], s[3])), s[7]), msg2[i]),
+    # )
+    func = lambda i: (
+        _xor(_xor(f(_xor(s[0], s[1])), s[4]), msg0[i]),
+        _xor(_xor(f(_xor(s[2], s[6])), s[7]), msg1[i]),
+        _xor(_xor(f(_xor(s[3], s[5])), s[8]), msg2[i]),
     )
-
     ct = []
     for i in range(msglen):
         ct0, ct1, ct2 = func(i)
@@ -131,12 +153,32 @@ def outputtag1(s, adlen, msglen):
         y = _xor(y, s[i])
     return y
 
-def outputtag2(s, adlen, msglen):
+def outputtag2(s, adlen, msglen, k0, k1, f):
+    s[0] = _xor(s[0], k0)
+    s[1] = _xor(s[1], k0)
+    s[2] = _xor(s[2], k1)
+    s[3] = _xor(s[3], k1)
+    s[4] = _xor(s[4], k0)
+    s[5] = _xor(s[5], k0)
+    s[6] = _xor(s[6], k1)
+    s[7] = _xor(s[7], k0)
+    s[8] = _xor(s[8], k1)
     for i in range(20):
-        roundupdate2(s, int2bits(adlen, 128), int2bits(msglen, 128), _z0, f)
+        roundupdate2(s, k0, _z0, k1, f)
+    s[0] = _xor(s[0], k1)
+    s[1] = _xor(s[1], k0)
+    s[2] = _xor(s[2], k0)
+    s[3] = _xor(s[3], k0)
+    s[4] = _xor(s[4], k1)
+    s[5] = _xor(s[5], k0)
+    s[6] = _xor(s[6], k0)
+    s[7] = _xor(s[7], k1)
+    s[8] = _xor(s[8], k1)
     y = s[0]
-    for i in range(1, 10):
+    for i in range(1, 9):
         y = _xor(y, s[i])
+    for i in range(9):
+        print(bits2hex(s[i]))
     return y
 
 def encrypt0(n, ad, msg, k, f):
@@ -184,22 +226,53 @@ def encrypt2(n, ad, msg, k, f):
     msg2 = [ msg[i][256:384] for i in range(msglen) ]
 
     ct = []
-    
+
+    s = initialization2(n, k0, k1, f)
+    # for i in range(9):
+    #     print(bits2hex(s[i]))
+    if adlen > 0:
+        absorbad2(s, ad0, ad1, ad2, adlen, f)
+    if msglen > 0:
+        ct = absorbmsg2(s, msg0, msg1, msg2, msglen, f)
+
+    t = outputtag2(s, adlen, msglen, k0, k1, f)
+    return ct, t
+
+def decrypt2(n, ad, msg, k, f):
+    k0, k1 = k[0:128], k[128:256]
+    adlen, msglen = len(ad), len(msg)
+    ad0 = [ad[i][0:128] for i in range(adlen)]
+    ad1 = [ad[i][128:256] for i in range(adlen)]
+    ad2 = [ad[i][256:384] for i in range(adlen)]
+    msg0 = [msg[i][0:128] for i in range(msglen)]
+    msg1 = [msg[i][128:256] for i in range(msglen)]
+    msg2 = [msg[i][256:384] for i in range(msglen)]
+
+    ct = []
+
     s = initialization2(n, k0, k1, f)
     if adlen > 0:
         absorbad2(s, ad0, ad1, ad2, adlen, f)
     if msglen > 0:
         ct = absorbmsg2(s, msg0, msg1, msg2, msglen, f)
-    t = outputtag2(s, adlen, msglen, f) 
+
+    t = outputtag2(s, adlen, msglen, k0, k1, f)
     return ct, t
 
-n   = int2bits(0x000102030405060708090A0B0C0D0E0F, 128)
-k   = int2bits(0x000102030405060708090A0B0C0D0E0F000102030405060708090A0B0C0D0E0F, 256)
-ad  = int2bits(0x000102030405060708090A0B0C0D0E0F, 128)
-msg = int2bits(0x000102030405060708090A0B0C0D0E0F, 128)
+n   = int2bits(0x51af4471e8bcf6a2704d71163021f4fd, 128)
+k   = int2bits(0x1640224596795a4c54550546722fc76b16d3059dfc04066657a839d2d5be827b, 256)
+ad  = int2bits(0xe098499961971de22fec2235b24c1309bab0da98c3a386daa98d918b8cd88d5d2fbe54a2c06d135bf0fdc7cc81f47625, 384)
+msg = int2bits(0x84070aa5e8afb486da0561b6a1b8816478b67f4be34cc1f34a02ecce308132709556a6ca986a3d6d7f9c5bb40c99aa61, 384)
 # msg = int2bytes(0xAF803CE25906F1D19FB6C6804E06EA28AB178F457AF6B493B7439EC6D4290062, 256)
 # k   = int2bytes(0x9E3225A9F133B5DEA168F4E2851F072FCC00FCAA7CA62061717A48E52E29A3FA, 256)
 
-ct, t = encrypt0(n, [ad], [msg], k, f_enae)
-print(bits2hex(ct[0]), bits2hex(t))
-    
+ct, tag0 = encrypt2(n, [ad], [msg], k, f_enae)
+message, tag1 = decrypt2(n, [ad], ct, k, f_enae)
+
+# test = int2bits(0x51af4471e8bcf6a2704d71163021f4fd, 128)
+# print(test)
+# print(bits2hex(f_enae(test)))
+print(bits2hex(ct[0]), bits2hex(tag0))
+print(bits2hex(message[0]), bits2hex(tag1))
+# print(bits2hex(message), bits2hex(tag1))
+
